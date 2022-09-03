3747e2a07dd60d13732ccc8cf66575baba09dfac
Andrew Simonson <asimonson1125@gmail.com>
2022-09-03 18:01:59 -0400

    Added new projects/skills

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,83 +1,121 @@
-import React from 'react'
-import Skills from '../componets/Skills'
-import MyCarousel from '../componets/Carousel.js';
+import React from "react";
+import Skills from "../componets/Skills";
+import MyCarousel from "../componets/Carousel.js";
 // import img1 from '../assets/photos/itsa_me.JPG'
 // import img2 from '../assets/photos/me_robot.jpeg'
-import img3 from '../assets/photos/AcademicTeam.jpg'
-import img4 from '../assets/photos/hagerstownVex2019.jpg'
-import img5 from '../assets/photos/WeThePeople_Districts.jpg'
-import img6 from '../assets/photos/WeThePeople.jpg'
-import img7 from '../assets/photos/WeThePeople_NationalsSetup.jpg'
-
+import img3 from "../assets/photos/AcademicTeam.jpg";
+import img4 from "../assets/photos/hagerstownVex2019.jpg";
+import img5 from "../assets/photos/WeThePeople_Districts.jpg";
+import img6 from "../assets/photos/WeThePeople.jpg";
+import img7 from "../assets/photos/WeThePeople_NationalsSetup.jpg";
 
 export default function AboutMe() {
-    return (
-        <div className='foreground'>
-            <div className='col'>
-                <div id='aboutMe' data-aos='fade-up'>
-                    <h2 className='concentratedHead'>About Me</h2>
-                    <p>I'm Andrew Simonson, a second year (third year standing) student at <strong>Rochester Institute of Technology </strong>
-                        in the <b>Computer Science</b> BS program, pursuing a career in data
-                        science with a focus on predictive analytics.
-                    </p>
-                    <p>When I'm not in class, you can find me working on extra collaborative projects,
-                        building discord bots, playing chess, reading up on some recent geopolitical
-                        development, or haphazardly expanding my list of unusable, yet bizzarely wise quotes.
-                    </p>
-                    <p>My goal is to strive to make the biggest positive impact on the world that I
-                        can.  I'm here to improve and optimize what we have so that we can spend more
-                        time on the things that matter.
-                    </p>
-                    <p>I also value the ability to understand the world at large.  That awareness is
-                        what grounds a person into reality and gives their place in the world value.
-                        My favorite part of learning about grographies and cultures is
-                        when I can justify to myself the hidden causality to historic events, which
-                        presents an inside look into how we can act in the present.
-                    </p>
-                    <p>
-                        It is my belief that being able to channel each distinct creative interest
-                        into a final product is vital.  With this philosophy that promotes dedication
-                        and enables an in-depth understanding, I can take pride in each of my projects,
-                        bridging the gap between the the multifaceted purpose present in everything around us.
-                    </p>
-                    {/*<img src={img1} alt='Me' className='boxedImg' />*/}
-                </div>
-            </div>
-            <div className='col'>
-                <div id='skills' data-aos='fade-up'>
-                    <h2>Skills</h2>
-                    <Skills>{['Python', 'JavaScript', 'Java', 'C', 'C++', 'Processing', 'P5.js', 'SQL', 'SQLite', 'Sequelize', 'SQLAlchemy',
-                        'HTML', 'CSS', 'LaTeX', 'Object-Oriented Programming', 'Git', 'Github', 'Linux', 'Raspberry Pi',
-                        'Angular', 'Flask', 'Jinja',  'DOM Scraping', 'Google API', 'React', 'Node.js']}</Skills>
-                </div>
-                <div data-aos='fade-up' className='elementBlock'>
-                    <h2>Extracirricular Awards</h2>
-                    <MyCarousel className='carousel' data-aos='fade-up'>
-                        {[
-                            {
-                                original: img4,
-                                description: "High School VEX Robotics President and Team Leader"
-                            },
-                            {
-                                original: img5,
-                                description: "We the People Civics Competition District Champions"
-                            },
-                            {
-                                original: img6,
-                                description: "We The People State Civics Competition Champions"
-                            },
-                            {
-                                original: img7,
-                                description: "what it looks like to compete in We the People Nationals, circa 2020"
-                            },
-                            {
-                                original: img3,
-                                description: "Academic Team county championships"
-                            }
-                        ]}
-                    </MyCarousel>
-                </div>
-            </div>
+  return (
+    <div className="foreground">
+      <div className="col">
+        <div id="aboutMe" data-aos="fade-up">
+          <h2 className="concentratedHead">About Me</h2>
+          <p>
+            I'm Andrew Simonson, a second year (third year standing) student at{" "}
+            <strong>Rochester Institute of Technology </strong>
+            in the <b>Computer Science</b> BS program, pursuing a career in data
+            science with a focus on predictive analytics.
+          </p>
+          <p>
+            When I'm not in class, you can find me working on extra
+            collaborative projects, building discord bots, playing chess,
+            reading up on some recent geopolitical development, or haphazardly
+            expanding my list of unusable, yet bizzarely wise quotes.
+          </p>
+          <p>
+            My goal is to strive to make the biggest positive impact on the
+            world that I can. I'm here to improve and optimize what we have so
+            that we can spend more time on the things that matter.
+          </p>
+          <p>
+            I also value the ability to understand the world at large. That
+            awareness is what grounds a person into reality and gives their
+            place in the world value. My favorite part of learning about
+            grographies and cultures is when I can justify to myself the hidden
+            causality to historic events, which presents an inside look into how
+            we can act in the present.
+          </p>
+          <p>
+            It is my belief that being able to channel each distinct creative
+            interest into a final product is vital. With this philosophy that
+            promotes dedication and enables an in-depth understanding, I can
+            take pride in each of my projects, bridging the gap between the the
+            multifaceted purpose present in everything around us.
+          </p>
+          {/*<img src={img1} alt='Me' className='boxedImg' />*/}
         </div>
-    )
+      </div>
+      <div className="col">
+        <div id="skills" data-aos="fade-up">
+          <h2>Skills</h2>
+          <Skills>
+            {[
+              "Python",
+              "JavaScript",
+              "Java",
+              "C",
+              "C++",
+              "MIPS Assembly",
+              "Processing",
+              "P5.js",
+              "SQL",
+              "PostgreSQL",
+              "SQLAlchemy",
+              "HTML",
+              "CSS",
+              "LaTeX",
+              "ArcGIS",
+              "Git",
+              "Github",
+              "Linux",
+              "OKD4",
+              "Kubernetes",
+              "Angular",
+              "Flask",
+              "Jinja",
+              "DOM Scraping",
+              "Google API",
+              "React",
+              "Node.js",
+            ]}
+          </Skills>
+        </div>
+        <div data-aos="fade-up" className="elementBlock">
+          <h2>Extracirricular Awards</h2>
+          <MyCarousel className="carousel" data-aos="fade-up">
+            {[
+              {
+                original: img4,
+                description:
+                  "High School VEX Robotics President and Team Leader",
+              },
+              {
+                original: img5,
+                description:
+                  "We the People Civics Competition District Champions",
+              },
+              {
+                original: img6,
+                description: "We The People State Civics Competition Champions",
+              },
+              {
+                original: img7,
+                description:
+                  "what it looks like to compete in We the People Nationals, circa 2020",
+              },
+              {
+                original: img3,
+                description: "Academic Team county championships",
+              },
+            ]}
+          </MyCarousel>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,56 +3,80 @@ import Project from '../componets/Project'
 import github from '../assets/github.svg'
 import globe from '../assets/globe.svg'
 
-export default function Projects(){
-    return(
-      <div className='foreground'>
-        <div data-aos='fade-up'>
+export default function Projects() {
+  return (
+    <div className='foreground'>
+      <div data-aos='fade-up'>
         <h2 className='concentratedHead'>Projects</h2>
-          <p>Project status is indicated by the color of the project card:<br /><span className='complete'>⬤</span> - Complete<br />
-            <span className='WIP'>⬤</span> - Work In Progress<br /><span className='incomplete'>⬤</span> - Incomplete
-          </p>
-        </div>
-        <ProjectList>
-          <Project status='WIP' title='Digital Portfolio' links={[['https://github.com/asimonson1125/asimonson1125.github.io', <img alt='github' src={github} />]]} >
-            A personal portfolio site made with React.js.
-          </Project>
-          <Project status='complete' title='Slate' links={[['https://github.com/asimonson1125/Slate', <img alt='github' src={github} />], ["https://slate.cs.house", <img alt='site' src={globe} />]]} >
-            Slate is a web app designed to help event coordinators schedule events by congregating participant calendar data. <br />
-            Ver 2.0 WIP - includes analytics, faster processing, and better UI
-          </Project>
-          <Project status='complete' title='Resume' links={[['https://github.com/asimonson1125/Resume', <img alt='github' src={github} />]]} >
-            My Resume, made in LaTeX with a custom design derived by the AltaCV template on OverLeaf.
-          </Project>
-          <Project status='complete' title='Humans vs. Zombies Bot' links={[['https://github.com/asimonson1125/HvZ-bot', <img alt='github' src={github} />]]} >
-            A Discord bot to handle role management and statistics for RIT's Humans vs. Zombies games.
-          </Project>
-          <Project status ='WIP' title='FinTech' links={[['https://github.com/LukeHorigan/Financial-Management-Assocation-', <img alt='github' src={github} />]]}  >
-            A team derived from the RIT Financial Management Association dedicated to learning about financial management of equities using automated solutions developed by students.
-          </Project>
-          <Project status='complete' title='Browser Trivia Bot'>
-            A tampermonkey tool used to automatically answer and submit online trivia forms, which can be tailored to different site layouts.<br /><br />
-            Source is currently private for competition.
-          </Project>
-          <Project status='complete' title='Querist' links={[['https://github.com/asimonson1125/Querist', <img alt='github' src={github} />]]} >
-            A modular discord bot to manage class discord servers divided by class sections.  Used in my Web and Mobile 101 class discord server.
-          </Project>
-          <Project status='complete' title='Acumen' links={[['https://github.com/asimonson1125/Acumen', <img alt='github' src={github} />]]} >
-            A personal Discord bot focused on statistical insight and role management for NationsGame, including NG Rolls Sim accessibility.
-          </Project>
-          <Project status='complete' title='NationsGame Rolls Sim' links={[['https://github.com/asimonson1125/NG-Rolls-Simulator', <img alt='github' src={github} />]]} >
-            A simulator for the browser game, NationsGame, to analyze unit composition and predict in-game victors and unit statistics.<br />
-            Unfortunately, NationsGame is now defunct.  Limited screenshots of functionality.
-          </Project>
-          <Project status='incomplete' title='WallCycle' links={[['https://github.com/asimonson1125/WallCycle', <img alt='github' src={github} />]]} >
-            A GNOME extension that cycles through a folder of wallpapers.
-          </Project>
-          <Project status='complete' title='VEXcode Button Engine' links={[['https://github.com/asimonson1125/VEXcode-Button-Generator', <img alt='github' src={github} />]]} >
-            VEXcode button library + examples and template for the VEX V5 brain 
-          </Project>
-          <Project status='complete' title='WinKeylogger' links={[['https://github.com/asimonson1125/WinKeylogger', <img alt='github' src={github} />]]}>
-            A C++ keylogger for windows based off a udemy course with my custom modifications and powershell script.
-          </Project>
-        </ProjectList>
+        <p>Project status is indicated by the color of the project card:<br /><span className='complete'>⬤</span> - Complete<br />
+          <span className='WIP'>⬤</span> - Work In Progress<br /><span className='incomplete'>⬤</span> - Incomplete
+        </p>
       </div>
-    )
+      <h3 className='concentratedHead'>Pinned:</h3>
+      <ProjectList>
+        <Project status='complete' title='Slate' links={[['https://github.com/asimonson1125/Slate', <img alt='github' src={github} />], ["https://slate.csh.rit.edu/", <img alt='site' src={globe} />]]} >
+          Slate is a web app designed to help event coordinators schedule events by congregating participant calendar data.  Includes Computer Science House account integration.
+        </Project>
+        <Project status='WIP' title='OccupyRIT' links={[['https://github.com/asimonson1125/Occupy-RIT', <img alt='github' src={github} />]]}>
+          Collects RIT Gym Occupancy data for analysis, determining busiest times.
+        </Project>
+        <Project status='WIP' title='Digital Portfolio' links={[['https://github.com/asimonson1125/asimonson1125.github.io', <img alt='github' src={github} />]]} >
+          A personal portfolio site made with React.js.  Might overhaul.
+        </Project>
+        <Project status='WIP' title='FinTech' links={[['https://github.com/LukeHorigan/Financial-Management-Assocation-', <img alt='github' src={github} />]]}  >
+          A team derived from the RIT Financial Management Association dedicated to learning about financial management of equities using automated solutions developed by students.
+        </Project>
+      </ProjectList>
+      <h3 className='concentratedHead'>All:</h3>
+      <ProjectList>
+        <Project status='WIP' title='CSH ImagineRIT2023'>
+          An ongoing project with the Computer Science House using bluetooth tracking to monitor the ImagineRIT event
+        </Project>
+        <Project status='WIP' title='OccupyRIT' links={[['https://github.com/asimonson1125/Occupy-RIT', <img alt='github' src={github} />]]}>
+          Collects RIT Gym Occupancy data for analysis, determining busiest times.
+        </Project>
+        <Project status='complete' title='Chesscom Embeds' links={[['https://github.com/asimonson1125/chesscom-embed', <img alt='github' src={github} />]]} >
+          A template for creating Chess.com user profile embeds
+        </Project>
+        <Project status='complete' title='Resume' links={[['https://github.com/asimonson1125/Resume', <img alt='github' src={github} />]]} >
+          My Resume, made in LaTeX with a custom design derived by the AltaCV template on OverLeaf.
+        </Project>
+        <Project status='WIP' title='Digital Portfolio' links={[['https://github.com/asimonson1125/asimonson1125.github.io', <img alt='github' src={github} />]]} >
+          A personal portfolio site made with React.js.  Might overhaul.
+        </Project>
+        <Project status='complete' title='Slate' links={[['https://github.com/asimonson1125/Slate', <img alt='github' src={github} />], ["https://slate.csh.rit.edu/", <img alt='site' src={globe} />]]} >
+          Slate is a web app designed to help event coordinators schedule events by congregating participant calendar data.  Includes Computer Science House account integration.
+        </Project>
+        <Project status='complete' title='Humans vs. Zombies Bot' links={[['https://github.com/asimonson1125/HvZ-bot', <img alt='github' src={github} />]]} >
+          A Discord bot to handle role management and statistics for RIT's Humans vs. Zombies games.
+        </Project>
+        <Project status='WIP' title='FinTech' links={[['https://github.com/LukeHorigan/Financial-Management-Assocation-', <img alt='github' src={github} />]]}  >
+          A team derived from the RIT Financial Management Association dedicated to learning about financial management of equities using automated solutions developed by students.
+        </Project>
+        <Project status='complete' title='Browser Trivia Bot'>
+          A tampermonkey tool used to automatically answer and submit online trivia forms, which can be tailored to different site layouts.<br /><br />
+          Source is currently private.
+        </Project>
+        <Project status='complete' title='Querist' links={[['https://github.com/asimonson1125/Querist', <img alt='github' src={github} />]]} >
+          A modular discord bot to manage class discord servers divided by class sections.  Used in my Web and Mobile 101 class discord server.
+        </Project>
+        <Project status='complete' title='Acumen' links={[['https://github.com/asimonson1125/Acumen', <img alt='github' src={github} />]]} >
+          A personal Discord bot focused on statistical insight and role management for NationsGame, including NG Rolls Sim accessibility.
+        </Project>
+        <Project status='complete' title='NationsGame Rolls Sim' links={[['https://github.com/asimonson1125/NG-Rolls-Simulator', <img alt='github' src={github} />]]} >
+          A simulator for the browser game, NationsGame, to analyze unit composition and predict in-game victors and unit statistics.<br />
+          Unfortunately, NationsGame is now defunct.  Limited screenshots of functionality.
+        </Project>
+        <Project status='incomplete' title='WallCycle' links={[['https://github.com/asimonson1125/WallCycle', <img alt='github' src={github} />]]} >
+          A GNOME extension that cycles through a folder of wallpapers.
+        </Project>
+        <Project status='complete' title='VEXcode Button Engine' links={[['https://github.com/asimonson1125/VEXcode-Button-Generator', <img alt='github' src={github} />]]} >
+          VEXcode button library + examples and template for the VEX V5 brain
+        </Project>
+        <Project status='complete' title='WinKeylogger' links={[['https://github.com/asimonson1125/WinKeylogger', <img alt='github' src={github} />]]}>
+          A C++ keylogger for windows based off a udemy course with my custom modifications and powershell script.
+        </Project>
+      </ProjectList>
+    </div>
+  )
 }
