9beb7c62b827f6a848c73f5f576cf941919b6823
Andrew Simonson <asimonson1125@gmail.com>
2023-01-25 13:04:16 -0600

    temporary 'activities' page

---
+++ src/pages/Activities.js
@@ -0,0 +1,7 @@
+import Neon from '../componets/Neon'
+
+export default function ErrorNotFound() {
+    return (
+        <Neon color='#fff'><h1 className='neon'>ERROR 404</h1><br /><h3 className='neon'>URL Not Found</h3></Neon>
+    )
+}
